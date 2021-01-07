80417afdd433fb710e225030b7de9e789caa3a60
PoppleJIW <54989527+PoppleJIW@users.noreply.github.com>
2021-01-07 17:11:21 +0000

    Teardown lambda (#1)
    ucfs claimant api mysql interface lambda will operate to 'teardown' the database between tests to ensure it has the correct structure ahead of a new run.

--- README.md
+++ README.md
@@ -7,3 +7,13 @@ This repo is a base to create new non-Terraform repos, adding the githooks submo
 
 After cloning this repo, please run:  
 `make bootstrap`
+
+
+## Testing
+
+There are tox unit tests in the module. To run them, you will need the module tox installed with `pip install tox`, 
+then go to the root of the module and simply run `tox` to run all the unit tests.
+
+**You should always ensure they work before making a pull request for your branch.**
+
+**If tox has an issue with Python version you have installed, you can specify such as `tox -e py38`.**
---
+++ src/teardown_lambda/teardown.py
@@ -0,0 +1,68 @@
+import json
+import os
+
+from common import common, database
+
+logger = None
+
+base_copy_query = """
+CREATE TABLE IF NOT EXISTS {table}_stage LIKE {table}_old;
+INSERT INTO {table}_stage (id, data) SELECT id, data from {table}_old;
+"""
+
+# Copy tables
+copy_queries = [
+    # Claimant
+    base_copy_query.replace("{table}", "claimant"),
+    # Contract
+    base_copy_query.replace("{table}", "contract"),
+    # Statement
+    base_copy_query.replace("{table}", "statement"),
+]
+
+# Drop in-use tables
+drop_query = """
+DROP TABLE IF EXISTS claimant;
+DROP TABLE IF EXISTS contract;
+DROP TABLE IF EXISTS statement;
+"""
+
+# Remove _stage suffixes
+rename_query = """
+RENAME TABLE claimant_stage TO claimant,
+             contract_stage TO contract,
+             statement_stage TO statement;
+"""
+
+
+def handler(event, context):
+    global logger
+
+    try:
+        # Empty list means no keys are required
+        args = common.get_parameters(event, [])
+
+        logger = common.initialise_logger(args)
+        connection = database.get_connection(args)
+
+        for query in copy_queries:
+            database.execute_multiple_statements(query, connection)
+
+        database.execute_multiple_statements(drop_query, connection)
+        database.execute_statement(rename_query, connection)
+
+        return 200
+    except Exception as e:
+        logger.error("Failed to execute one or more SQL statement(s)")
+        logger.error(e)
+        return 500
+
+
+if __name__ == "__main__":
+    script_dir = os.path.dirname(__file__)
+    rel_path = "../resources"
+    abs_file_path = os.path.join(script_dir, rel_path)
+
+    with open(os.path.join(abs_file_path, "event.json"), "r") as file:
+        json_content = json.loads(file.read())
+        handler(json_content, None)
